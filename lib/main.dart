21ff23a0a16e9b1b63da33ca6a48ca7214957447
Cathrrr <oktavianicatherine@gmail.com>
2024-09-12 14:30:59 +0700

    Menambahkan file txt untuk tugas

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Catherine-220711939'),
         ),
       ),
     );
