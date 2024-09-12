1638a98a652c7bbc82708d3c70da9f0fad59102b
staryone <stanyslausharyone@gmail.com>
2024-09-12 14:30:10 +0700

    Menambahkan text pada main dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Stanyslaus Hary Muntoro - 220712140'),
         ),
       ),
     );
