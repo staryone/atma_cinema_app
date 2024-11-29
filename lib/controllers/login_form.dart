2fc0f160c5cf99e956fe366557706876c74edd32
Stanyslaus Hary Muntoro <67821697+staryone@users.noreply.github.com>
2024-11-29 20:46:18 +0700

    feat: menambahkan circular progress pada loading login

--- lib/controllers/login_form.dart
+++ lib/controllers/login_form.dart
@@ -148,157 +148,166 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    Map? dataForm = {};
-
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(25),
-          topRight: Radius.circular(25),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 10,
-            offset: Offset(0, -2),
-          ),
-        ],
-      ),
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          SizedBox(height: 18),
-          ElevatedButton.icon(
-            onPressed: _loginWithGoogle,
-            icon: SvgPicture.asset('images/googleIcon.svg'),
-            label: Text("Login with Google"),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              foregroundColor: colorPrimary,
-              minimumSize: Size(350, 48.9),
-              side: BorderSide(color: colorBorder),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(7),
-              ),
-              textStyle: GoogleFonts.roboto(
-                textStyle: styleBold,
-              ),
+    return Stack(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25),
+              topRight: Radius.circular(25),
             ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
+                offset: Offset(0, -2),
+              ),
+            ],
           ),
-          const SizedBox(height: 21.5),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Divider(color: colorBorder, thickness: 1),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(height: 18),
+              ElevatedButton.icon(
+                onPressed: _loginWithGoogle,
+                icon: SvgPicture.asset('images/googleIcon.svg'),
+                label: Text("Login with Google"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: colorPrimary,
+                  minimumSize: Size(350, 48.9),
+                  side: BorderSide(color: colorBorder),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                  textStyle: GoogleFonts.roboto(
+                    textStyle: styleBold,
+                  ),
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Text("or"),
-                ),
-                Expanded(
-                  child: Divider(color: colorBorder, thickness: 1),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 18),
-          Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                InputForm(
-                  validasi: (p0) {
-                    if (p0 == null || p0.isEmpty) {
-                      return "Email tidak boleh kosong";
-                    }
-                    if (!p0.contains('@') && !p0.contains('.')) {
-                      return "Masukkan email yang valid";
-                    }
-                    return null;
-                  },
-                  controller: _emailController,
-                  hintTxt: "example@gmail.com",
-                  labelTxt: "Email",
-                ),
-                InputForm(
-                  validasi: (p0) {
-                    if (p0 == null || p0.isEmpty) {
-                      return "Password tidak boleh kosong";
-                    }
-                    if (p0.length < 5) {
-                      return "Password minimal 5 digit";
-                    }
-                    return null;
-                  },
-                  controller: _passwordController,
-                  hintTxt: "**********",
-                  iconData: Icons.visibility_off,
-                  password: true,
-                  labelTxt: "Password",
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ),
+              const SizedBox(height: 21.5),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
                   children: [
-                    ElevatedButton(
-                      onPressed: _login,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: colorPrimary,
-                        foregroundColor: Colors.white,
-                        minimumSize: Size(350, 48.9),
-                        side: BorderSide(color: colorBorder),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(7),
-                        ),
-                        textStyle: styleBold,
-                      ),
-                      child: Text('Login'),
+                    Expanded(
+                      child: Divider(color: colorBorder, thickness: 1),
                     ),
-                    SizedBox(height: 75),
-                    TextButton(
-                      onPressed: () {
-                        // Map<String, dynamic> formData = {};
-                        // formData['email'] = _emailController.text;
-                        // formData['password'] = _passwordController.text;
-                        pushRegister(context);
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text("or"),
+                    ),
+                    Expanded(
+                      child: Divider(color: colorBorder, thickness: 1),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 18),
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    InputForm(
+                      validasi: (p0) {
+                        if (p0 == null || p0.isEmpty) {
+                          return "Email tidak boleh kosong";
+                        }
+                        if (!p0.contains('@') && !p0.contains('.')) {
+                          return "Masukkan email yang valid";
+                        }
+                        return null;
                       },
-                      style: TextButton.styleFrom(
-                        foregroundColor: Colors.black,
-                        textStyle: styleMedium,
-                      ),
-                      child: RichText(
-                        text: TextSpan(
-                          text: "Don't have an account? ",
-                          style: const TextStyle(
-                            color: Colors.black,
-                          ),
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Register',
-                              style: GoogleFonts.roboto(
-                                textStyle: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: colorPrimary,
-                                ),
-                              ),
+                      controller: _emailController,
+                      hintTxt: "example@gmail.com",
+                      labelTxt: "Email",
+                    ),
+                    InputForm(
+                      validasi: (p0) {
+                        if (p0 == null || p0.isEmpty) {
+                          return "Password tidak boleh kosong";
+                        }
+                        if (p0.length < 5) {
+                          return "Password minimal 5 digit";
+                        }
+                        return null;
+                      },
+                      controller: _passwordController,
+                      hintTxt: "**********",
+                      iconData: Icons.visibility_off,
+                      password: true,
+                      labelTxt: "Password",
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: _login,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: colorPrimary,
+                            foregroundColor: Colors.white,
+                            minimumSize: Size(350, 48.9),
+                            side: BorderSide(color: colorBorder),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(7),
                             ),
-                          ],
+                            textStyle: styleBold,
+                          ),
+                          child: Text('Login'),
                         ),
-                      ),
+                        SizedBox(height: 75),
+                        TextButton(
+                          onPressed: () {
+                            // Map<String, dynamic> formData = {};
+                            // formData['email'] = _emailController.text;
+                            // formData['password'] = _passwordController.text;
+                            pushRegister(context);
+                          },
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            textStyle: styleMedium,
+                          ),
+                          child: RichText(
+                            text: TextSpan(
+                              text: "Don't have an account? ",
+                              style: const TextStyle(
+                                color: Colors.black,
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Register',
+                                  style: GoogleFonts.roboto(
+                                    textStyle: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: colorPrimary,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        if (_isLoading)
+          Container(
+            color: Colors.black,
+            height: MediaQuery.of(context).size.height,
+            alignment: Alignment.center,
+            child: const CircularProgressIndicator(),
+          ),
+      ],
     );
   }
 
